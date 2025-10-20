a965579e82b02445220b26278d838a3da56f0d2c
Ralf Biedert <rb@xr.io>
2025-10-20 19:52:25 +0200

    Make it expect.

--- crates/fundle_macros_impl/src/lib.rs
+++ crates/fundle_macros_impl/src/lib.rs
@@ -5,7 +5,7 @@
 //!
 //! See the `fundle_macros` crate for more information.
 
-#![allow(clippy::missing_panics_doc, clippy::missing_errors_doc, reason = "This is a macro")]
+#![expect(clippy::missing_panics_doc, clippy::missing_errors_doc, reason = "This is a macro")]
 
 mod bundle;
 mod deps;
