5240ced8fb18fb460aed4d7c2784ffa694d1c71b
Ralf Biedert <rb@xr.io>
2025-12-10 19:50:19 +0100

    That wasn't it.

--- crates/thread_aware/src/registry.rs
+++ crates/thread_aware/src/registry.rs
@@ -209,7 +209,6 @@ mod tests {
     }
 
     #[test]
-    #[expect(clippy::assertions_on_constants, reason = "Testing codecov issue")]
     fn test_crate_fake_memory_affinities() {
         let affinities = create_manual_memory_affinities(&[2, 3]);
         assert_eq!(affinities.len(), 5);
@@ -219,8 +218,6 @@ mod tests {
                 assert_eq!(affinity.processor_count(), 5);
                 assert_eq!(affinity.memory_region_index(), usize::from(i >= 2));
                 assert_eq!(affinity.memory_region_count(), 2);
-            } else {
-                assert!(true); // ?!
             }
         }
     }
