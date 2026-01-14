27ef6f1c9cbd707f957619602db8f72a5875ad14
Vaiz <4908982+Vaiz@users.noreply.github.com>
2026-01-14 10:10:16 +0100

    rename app_err feature to app-err

--- crates/ohno/src/app/macros.rs
+++ crates/ohno/src/app/macros.rs
@@ -44,7 +44,7 @@
 /// }
 /// ```
 #[macro_export]
-#[cfg_attr(docsrs, doc(cfg(feature = "app_err")))]
+#[cfg_attr(docsrs, doc(cfg(feature = "app-err")))]
 macro_rules! bail {
     ($msg:literal $(,)?) => {
         return Err($crate::AppError::new(format!($msg)))
@@ -98,7 +98,7 @@ macro_rules! bail {
 /// }
 /// ```
 #[macro_export]
-#[cfg_attr(docsrs, doc(cfg(feature = "app_err")))]
+#[cfg_attr(docsrs, doc(cfg(feature = "app-err")))]
 macro_rules! app_err {
     ($msg:literal $(,)?) => {
         $crate::AppError::new(format!($msg))
