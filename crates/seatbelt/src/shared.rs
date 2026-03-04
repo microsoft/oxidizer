968f8e334b5364b90a29adca740b7be872ee192e
martintomka <martintomka@microsoft.com>
2026-03-04 08:25:23 +0100

    chore(seatbelt): get rid of warnings caused by hidden docs

--- crates/seatbelt/src/shared.rs
+++ crates/seatbelt/src/shared.rs
@@ -4,11 +4,9 @@
 /// A flag indicating that the required property is set.
 #[non_exhaustive]
 #[derive(Debug)]
-#[doc(hidden)]
 pub struct Set;
 
 /// A flag indicating that the required property has not been set.
 #[non_exhaustive]
 #[derive(Debug)]
-#[doc(hidden)]
 pub struct NotSet;
